4f6f5044751a10215f4c6c965fef25712309a2a6
LUIZ CODE <beat.luiz23@hotmail.com>
2024-09-28 15:10:53 -0300

    att

--- public/pagamento.js
+++ public/pagamento.js
@@ -11,14 +11,54 @@ function adicionar_mais() {
     
 
 }
+function formatarDinheiro(input) {
+    // Remove tudo que não é número
+    let valor = input.value.replace(/\D/g, '');
+
+    // Formata o valor como dinheiro
+    valor = (valor / 100).toFixed(2).replace('.', ',');
+    valor = valor.replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+
+    // Atualiza o campo de entrada
+    input.value = valor;
+}
+
+
+
+// const numeroOriginal = 1234900; // equivalente a R$ 12.349,00
+// const numeroFormatado = formatarNumero(numeroOriginal);
+// console.log(numeroFormatado); // Saída: "12.349,00"
+
+
+function pagamento_data_hora() {
+    const agora = new Date();
+
+        // Obtém a data e a hora
+        const data = agora.toLocaleDateString('pt-BR'); // Formato da data
+        const hora = agora.toLocaleTimeString('pt-BR'); // Formato da hora
+
+        // Mostra no HTML
+        document.querySelector('.data').innerText = `Data: ${data}`;
+        document.querySelector('.hora').innerText = `Hora: ${hora}`;
+
+}
+pagamento_data_hora();
+        setInterval(pagamento_data_hora, 1000);
+
 
 function option() {
-    const option=document.querySelector('.option')
 
-    const pegar_option =option.value
-
-    alert("o valor da celeção"+pegar_option);
-
+     // Pega todos os inputs dentro da div de resultado
+    const inputs = document.querySelectorAll('.option');
+    for (let index1 = 0; index1 < inputs.length; index1++) {
+        
+        const terceiroValor = inputs[index1].value; // Acessa o valor do terceiro input
+        
+        document.querySelector('.mostrar2').textContent += terceiroValor; // Mostra no HTML
+        
+        // alert(terceiroValor);           
+        
+                }
 }
 
 
@@ -32,9 +72,10 @@ function quantidade() {
  const terceiroValor = inputs[index].value; // Acessa o valor do terceiro input
         document.querySelector('.mostrar3').textContent += terceiroValor; // Mostra no HTML
 
-
-        }
+        
     }
+}
+
 
         
 function pagamento_nome() {
@@ -46,6 +87,26 @@ function pagamento_nome() {
         const terceiroValor = inputs[index1].value; // Acessa o valor do terceiro input
         
         document.querySelector('.mostrar1').textContent += terceiroValor; // Mostra no HTML
+        
+        // alert(terceiroValor);           
+        
+                }
+
+
+
+    
+  
+
+}
+function pagamento_valor() {
+    
+    // Pega todos os inputs dentro da div de resultado
+    const inputs = document.querySelectorAll('.pagamento_valor');
+    for (let index1 = 0; index1 < inputs.length; index1++) {
+        
+        const terceiroValor = inputs[index1].value; // Acessa o valor do terceiro input
+        
+        document.querySelector('.mostrar4').textContent += terceiroValor; // Mostra no HTML
         
         // alert(terceiroValor);           
         
@@ -69,7 +130,7 @@ function salvar() {
         quantidade();
         pagamento_nome();
         option()
-       
+        pagamento_valor()
 
 
 
