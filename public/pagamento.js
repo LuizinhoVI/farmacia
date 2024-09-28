111c485aadfe8c89d4bb18f5cdc3cee8af5ec43a
LUIZ CODE <beat.luiz23@hotmail.com>
2024-09-27 22:39:45 -0300

    att

--- public/pagamento.js
+++ public/pagamento.js
@@ -1,20 +1,9 @@
-let id =0;
+let id =1;
 function adicionar_mais() {
    
-    // const produto = document.createElement("div");
-    // produto.className='w3-container';
-    // produto.
-
-
-   
-    
-    
-    
-
-
-    // faz criar um clone da div 
-    const mais = document.querySelector("#adicionar_produto");
     document.querySelector('.contador_de_id').innerHTML=id++;
+    
+    const mais = document.querySelector("#adicionar_produto");
     const copiar = mais.cloneNode(true);
 
     document.getElementById("colar").appendChild(copiar);
@@ -24,21 +13,57 @@ function adicionar_mais() {
 }
 
 
+function quantidade() {
+
+    // Pega todos os inputs dentro da div de resultado
+    const inputs = document.querySelectorAll('.pagamento_quantidade');
+        for (let index = 0; index <= inputs.length; index++) {
+
+            
+ const terceiroValor = inputs[index].value; // Acessa o valor do terceiro input
+        document.querySelector('.mostrar3').textContent += terceiroValor; // Mostra no HTML
+
+
+        }
+    }
+        function pagamento_nome() {
+
+            // Pega todos os inputs dentro da div de resultado
+            const inputs = document.querySelectorAll('.pagamento_quantidade');
+                for (let index = 0; index <= inputs.length; index++) {
+        
+                    
+         const terceiroValor = inputs[index].value; // Acessa o valor do terceiro input
+                document.querySelector('.mostrar3').textContent += terceiroValor; // Mostra no HTML
+        
+        
+                }
+
+
+    // Se houver pelo menos 3 inputs, acessa o valor do terceiro
+    if (inputs.length >= 3) {
+
+
+        const terceiroValor = inputs[2].value; // Acessa o valor do terceiro input
+        document.querySelector('.mostrar3').textContent = terceiroValor; // Mostra no HTML
+    }
+    
+  
+
+}
+
 
 function salvar() {
     const checkbox = document.querySelector('.verificar_compra');
     // console.log('Checkbox encontrado:', checkbox); 
     if (checkbox.checked) {
         // console.log('Checkbox está marcado:', checkbox.checked);
-     
 
-
-       const tipo = document.querySelectorAll('nome_procudo');
-
+        quantidade();
+        pagamento_nome();
 
        
 
-        console.log(tipo[1].textcontent);
 
 
 
