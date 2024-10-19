2a39bb5d7def4dccfcd95c3c1717658f76078af7
LUIZ CODE <beat.luiz23@hotmail.com>
2024-10-19 11:56:18 -0300

    Revert "att"
    This reverts commit e0da2bde7bed357d3a8d2cd31e90b4e50fd13444.

--- public/Ficha.js
+++ public/Ficha.js
@@ -16,37 +16,3 @@ function ficha_menu(numero) {
     
     }
     
-    function formatarCep(input) {
-        // Remove todos os caracteres que não são números
-        let valor = input.value.replace(/\D/g, '');
-        // Adiciona o hífen na posição correta
-        if (valor.length > 5) {
-            valor = valor.slice(0, 5) + '-' + valor.slice(5, 8);
-        }
-        // Atualiza o valor do input
-        input.value = valor;
-    }
-    async function buscarCep() {
-        const cep = document.getElementById('cep').value.replace('-', '');
-        const cidade = document.getElementById('cidade');
-        const estado = document.getElementById('estado');
-        // resultadoDiv.innerHTML = ''; // Limpa o resultado anterior
-
-        if (cep.length === 8) {
-            
-                const response = await fetch(`https://brasilapi.com.br/api/cep/v1/${cep}`);
-                const data = await response.json();
-
-                if (!data.erro) {
-                //   alert(data.state)
-
-                    cidade.value=data.city  
-                    estado.value=data.state
-
-
-                } else {
-                   alert("não encontrou a cidade")
-                }
-            
-            }
-        }
