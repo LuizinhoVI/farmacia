ad3d69bdafc72cbc98d6fc1015cb54c442179184
LUIZ CODE <beat.luiz23@hotmail.com>
2024-11-04 12:08:43 -0300

    att

--- public/pesquisa.js
+++ public/pesquisa.js
@@ -1,13 +1,46 @@
 //const searchButton = document.getElementById('searchButton');
-        const resultFrame = document.getElementById('resultadoframe').value;
+        const resultFrame = document.getElementById('iframe');
 
 
-
+        
         function menu_pesquisa() {
             
             const query = document.getElementById('resultadoframe').value;
+            const mostrar_pesquisa = document.querySelector('.local_de_pesquisa');
+
+            if (query) {
             const url = `https://www.bing.com/images/search?q=`+query;
             resultFrame.src = url; // Atualiza o src do iframe para carregar os resultados
         console.log(   url );
+
+        mostrar_pesquisa.style.display= 'block'
+            
+            
+        }else{
+            alert('Por favor, insira alguma pesquisa.');
         }
+
+        }
+      
+            function realizarPesquisa(event) {
+                if (event.key === 'Enter') {
+                    
+                    const query = document.getElementById('resultadoframe').value;
+                    const mostrar_pesquisa = document.querySelector('.local_de_pesquisa');
+        
+                    if (query) {
+                    const url = `https://www.bing.com/images/search?q=`+query;
+                    resultFrame.src = url; // Atualiza o src do iframe para carregar os resultados
+                console.log(   url );
+        
+                mostrar_pesquisa.style.display= 'block'
+                    
+                    
+                }else{
+                    alert('Por favor, insira alguma pesquisa.');
+                }    
+                
+                }
+            }
+        
         
