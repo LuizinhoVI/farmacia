4c604835284732d175e41400e058cf98b5b21e52
LUIZ CODE <beat.luiz23@hotmail.com>
2024-11-07 09:55:13 -0300

    atualização

--- public/historico.js
+++ public/historico.js
@@ -28,31 +28,27 @@ function filterTable() {
             row.style.display = "none";
         }
         
+
+
+
+
+
+
+
     });
 }
-
-function formatarValor(numero) {
-    const nuem
-    numero.value.replace(/\D/g, '');
+// função para formatar a data -------------------------------------------
+function formatarData(input) {
+    let valor = input.value.replace(/\D/g, ''); // Remove todos os caracteres não numéricos
     
-    // let input = document.getElementById('filterValor');
-    // let value = input.value.replace(/\D/g, ''); // Remove qualquer caractere não numérico
-    
-    // // Se a string estiver vazia ou apenas com espaços
-    // if (value === '00') {
-    //     input.value = null;
-    //     return;
-    // }
-    
-    // // Formata o número para R$ 1.150,50 (inserindo ponto de milhar e vírgula para centavos)
-    // let valorFormatado = '' + (parseInt(value) / 100).toFixed(2).replace('.', ',');
-    
-    // // Adiciona o ponto de milhar
-    // valorFormatado = valorFormatado.replace(/\B(?=(\d{3})+(?!\d))/g, '.');
-    
-    // input.value = valorFormatado; // Atualiza o campo com o valor 
-    // console.log(input.value)
-    
-
-  
+    if (valor.length <= 2) {
+        input.value = valor; // Apenas o dia
+    } else if (valor.length <= 4) {
+        input.value = valor.slice(0, 2) + '/' + valor.slice(2); // Formato DD/MM
+    } else {
+        input.value = valor.slice(0, 2) + '/' + valor.slice(2, 4) + '/' + valor.slice(4, 6); // Formato DD/MM/AA
+    }
 }
+// --------------------------------------------------------------------
+
+
